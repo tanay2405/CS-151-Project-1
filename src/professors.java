6af1b19602d87392d6fbf7419d8c69068d41e35e
srihanrc <151196629+srihanrc@users.noreply.github.com>
2025-09-26 17:37:45 -0700

    Add files via upload

---
+++ src/professors.java
@@ -0,0 +1,69 @@
+package src;
+
+import java.util.*;
+import java.util.LinkedHashSet;
+
+public class professors {
+
+    private int professorID;
+    private String professorName;
+    private String department;
+    private String email;
+    private Set<Course> assignProfessor = new LinkedHashSet<>();
+
+    public professors(int professorID, String professorName, String department, String email) {
+        this.professorID = professorID;
+        this.professorName = professorName;
+        this.department = department;
+        this.email = email;
+    }
+    public void setProfessorID(int professorID) {
+        this.professorID = professorID;
+    }
+    public int getProfessorID() {
+        return professorID;
+    }
+    public void setProfessorName(String professorName) {
+        this.professorName = professorName;
+    }
+    public String getProfessorName() {
+        return professorName;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+    public String getDepartment() {
+        return department;
+    } 
+    public Set<Course> getCourses() {
+        return assignProfessor;
+    }  
+
+    public void addProfessor(Course c)
+    {
+        if (c != null)
+        {
+            assignProfessor.add(c);
+            c.setProfessorID(this);
+        }
+        
+    }
+
+    public void removeProfessor(Course c)
+    {
+        if (c != null && assignProfessor.contains(c))
+        {
+            assignProfessor.remove(c);
+            c.setProfessorID(null);
+        }   
+    }
+
+  
+    
+}
